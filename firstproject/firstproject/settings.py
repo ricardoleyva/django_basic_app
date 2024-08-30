c860ddee42915b4d3deec71da66bb156958a485f
Ricardo Leyva <ricardomtzleyva@hotmail.com>
2024-08-29 20:39:29 -0400

    Added Dockerfile and requirements.txt

--- firstproject/firstproject/settings.py
+++ firstproject/firstproject/settings.py
@@ -25,12 +25,13 @@ SECRET_KEY = 'django-insecure-^ttr=dz3gu^#kjp_$g()mvng##=y0y4t-*&^k9o1m(5c5geouh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'firstapp.apps.FirstappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
